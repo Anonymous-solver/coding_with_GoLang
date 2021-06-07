42050e858e66d22fe52d7a999380f581b3d6720e
Anonymous-solver <anik.uits29@gmail.com>
2021-06-07 15:06:45 +0600

    commit dateFormation

---
+++ dateFormation102.go
@@ -0,0 +1,21 @@
+// Golang program to illustrate the usage of
+// time.Parse() function
+  
+// Including main package
+package main
+  
+// Importing fmt and time
+import "fmt"
+import "time"
+  
+// Calling main
+func main() {
+  
+    // Declaring layout constant
+    const layout = "2 January 2006 3:04 PM"
+  
+    // Calling Parse() method with its parameters
+    tm, _ := time.Parse(layout, "4 May 2021 12:31 AM")
+    fmt.Println(tm.Format(time.RFC3339Nano))
+    
+}
